a175893bfc4c77ad25db7b9ed8164846a28aed62
ooyam <73095407+ooyam@users.noreply.github.com>
2021-08-18 00:19:40 +0900

    2021.08.18
    列消しエフェクト不具合修正
    タイトル画面オラフが野菜を持っていく演出実装

--- Assets/Script/Title/TitleHamster.cs
+++ Assets/Script/Title/TitleHamster.cs
@@ -10,15 +10,20 @@ public class TitleHamster : MonoBehaviour
     public Sprite[] hamsterSpr;
     [Header("•—‘Dsprite")]
     public Sprite[] balloonSpr;
+    private int spriteIndex;
+    private Collider2D col;
     private float moveSpeed = 3.0f;
     private float desPos = 1300.0f;
     private float posX;
     private float posY;
+    private bool vegCatch = false;
+    private VegetableMove vegScr;
 
     // Start is called before the first frame update
     void Start()
     {
         tra = GetComponent<RectTransform>();
+        col = GetComponent<Collider2D>();
         float width = 400.0f;
         posX = Random.Range(-width, width);
         posY = -desPos;
@@ -32,16 +37,22 @@ public class TitleHamster : MonoBehaviour
         else balloonObj.color = Color.white;
 
         float[] hamsterPosX = new float[] { -30.0f, 10.0f, 0.0f, -20.0f, -20.0f, -20.0f, -10.0f, -30.0f, -25.0f, -20.0f };
-        int hamsterIndex = Random.Range(0, hamsterSpr.Length);
+        spriteIndex = Random.Range(0, hamsterSpr.Length);
         GameObject hamsterObj = tra.GetChild(1).gameObject;
-        hamsterObj.GetComponent<Image>().sprite = hamsterSpr[hamsterIndex];
+        hamsterObj.GetComponent<Image>().sprite = hamsterSpr[spriteIndex];
         RectTransform hamsterTra = hamsterObj.GetComponent<RectTransform>();
-        hamsterTra.anchoredPosition = new Vector2(hamsterPosX[hamsterIndex], 0.0f);
-        switch (hamsterIndex)
+        hamsterTra.anchoredPosition = new Vector2(hamsterPosX[spriteIndex], 0.0f);
+        switch (spriteIndex)
         {
+            case 0:
+            case 1:
+            case 2:
+                col.enabled = true;
+                break;
             case 6:
                 hamsterTra.sizeDelta = new Vector2(130.0f, 150.0f);
                 hamsterTra.SetSiblingIndex(0);
+                col.enabled = true;
                 break;
             case 7:
             case 8:
@@ -58,7 +69,24 @@ public class TitleHamster : MonoBehaviour
         tra.anchoredPosition = new Vector2(posX, posY);
         if(posY > desPos)
         {
+            if (vegCatch) vegScr.RegenerationStart();
             Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject collObj = collider.gameObject;
+        if (!vegCatch && collObj.tag == "Vegetable")
+        {
+            RectTransform vegTra = collObj.GetComponent<RectTransform>();
+            vegScr = collObj.GetComponent<VegetableMove>();
+            vegTra.SetParent(tra, false);
+            float posX = (spriteIndex == 2) ? -30.0f : 0.0f;
+            float posY = (spriteIndex == 2) ? 0.0f : -30.0f;
+            vegTra.anchoredPosition = new Vector2(posX, posY);
+            vegCatch = true;
+            vegScr.hamCatch = true;
         }
     }
 }
--- Assets/Script/Title/VegetableMove.cs
+++ Assets/Script/Title/VegetableMove.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VegetableMove : MonoBehaviour
 {
     private RectTransform tra;
+    private Image ima;
+    private Collider2D coll;
+    private Transform parentTra;
+    private Vector2 startPos;
     private float moveSpeed = 3.5f;
     private float maxRot = 10.0f;
     private bool leftMove = true;
     private bool moveStop = false;
+    [System.NonSerialized]
+    public bool hamCatch = false;
 
     // Start is called before the first frame update
     void Start()
     {
         tra = GetComponent<RectTransform>();
+        ima = GetComponent<Image>();
+        coll = GetComponent<Collider2D>();
+        parentTra = tra.parent.gameObject.transform;
+        startPos = tra.anchoredPosition;
         float startRot = Random.Range(0.0f, 20.0f);
         tra.localRotation = Quaternion.Euler(0.0f, 0.0f, startRot - 10.0f);
         StartCoroutine(MoveStop());
@@ -22,7 +33,7 @@ public class VegetableMove : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!moveStop)
+        if (!moveStop && !hamCatch)
         {
             float rotZ = tra.localRotation.eulerAngles.z;
             rotZ = (rotZ >= 180.0f) ? rotZ - 360.0f : rotZ;
@@ -55,5 +66,32 @@ public class VegetableMove : MonoBehaviour
             moveStop = true;
             yield return new WaitForSeconds(waitTime);
         }
+    }
+
+    public void RegenerationStart()
+    {
+        tra.SetParent(parentTra, false);
+        StartCoroutine(Regeneration());
+    }
+
+    IEnumerator Regeneration()
+    {
+        hamCatch = false;
+        float alpha = 0.0f;
+        ima.color = new Color(1, 1, 1, alpha);
+        coll.enabled = false;
+        tra.anchoredPosition = startPos;
+        float stealthTime = 3.0f;
+        yield return new WaitForSeconds(stealthTime);
+
+        float oneFrametime = 0.02f;
+        while (true)
+        {
+            alpha += oneFrametime;
+            ima.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+            if (alpha >= 1.0f) break;
+            yield return new WaitForSeconds(oneFrametime);
+        }
+        coll.enabled = true;
     }
 }
