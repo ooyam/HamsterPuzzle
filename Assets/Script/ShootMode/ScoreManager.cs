788f6eb913fb9e72d6f133f78d1cca8c1e2221f5
ooyam <73095407+ooyam@users.noreply.github.com>
2021-12-28 23:06:31 +0900

    2021/12/28
    目標野菜の設定方法確立
    クリア判定実装

--- Assets/Script/ShootMode/ScoreManager.cs
+++ Assets/Script/ShootMode/ScoreManager.cs
@@ -4,27 +4,45 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using static ShootMode.ShootModeDefine;
+using static ShootMode.ShootModeManager;
 
 namespace ShootMode
 {
     public class ScoreManager : MonoBehaviour
     {
-        [Header("数字スプライト")]
+        [Header("スコア用数字スプライト")]
         [SerializeField]
-        Sprite[] numberSpr;
+        Sprite[] scoreNumSpr;
+
+        [Header("ターゲット用数字スプライト")]
+        [SerializeField]
+        Sprite[] targetNumSpr;
 
         [Header("スコア表示オブジェクト")]   //0：ブロッコリー　1:キャベツ　2：ニンジン　3:パプリカ　4：カボチャ　5：トウモロコシ
         [SerializeField]
         GameObject[] scoreObj;
-
         RectTransform[] scoreObjTra; //オブジェクトRectTransform
-        RectTransform[] numTra;      //収穫数RectTransform
-        Image[][] numIma;            //出力数字
-        int maxDigit = 2;            //最大桁数
+        RectTransform[] scoreNumTra; //収穫数RectTransform
+        Image[][] scoreNumIma;       //出力数字
 
-        string[] vegName; //野菜の名前
-        int vegTypeNum;   //使用する野菜の数
-        int[] harvestNum; //収穫個数
+        [Header("ターゲット表示オブジェクト")]   //0：ブロッコリー　1:キャベツ　2：ニンジン　3:パプリカ　4：カボチャ　5：トウモロコシ
+        [SerializeField]
+        GameObject[] targetObj;
+        RectTransform[] targetObjTra; //オブジェクトRectTransform
+        RectTransform[] targetNumTra; //収穫数RectTransform
+        Image[][] targetNumIma;       //出力数字
+        int maxDigit = 2;             //最大桁数
+
+        VegetableType[] tartgetVeg_;  //目標野菜
+        int targetVegetableNum_;      //目標野菜の個数
+        int[] targetNum_;             //種類ごとの目標個数
+        int stageNum_;                //ステージ番号
+
+        string[] vegName;  //野菜の名前
+        int vegTypeNum;    //使用する野菜の数
+        int[] harvestNum;  //収穫個数
+        int[] targetIndex; //目標野菜のインデクス番号
+        bool[] clearJudge; //クリア判定
 
         void Start()
         {
@@ -34,18 +52,47 @@ namespace ShootMode
             foreach (VegetableType vegeValue in vegetableType)
             { vegName[(int)vegeValue] = Enum.GetName(typeof(VegetableType), vegeValue); }
 
-            scoreObjTra = new RectTransform[vegTypeNum];
-            numTra      = new RectTransform[vegTypeNum];
-            numIma      = new Image[vegTypeNum][];
+            scoreObjTra  = new RectTransform[vegTypeNum];
+            scoreNumTra  = new RectTransform[vegTypeNum];
+            scoreNumIma  = new Image[vegTypeNum][];
+            targetObjTra = new RectTransform[vegTypeNum];
+            targetNumTra = new RectTransform[vegTypeNum];
+            targetNumIma = new Image[vegTypeNum][];
             for (int index = 0; index < vegTypeNum; index++)
             {
-                scoreObjTra[index] = scoreObj[index].GetComponent<RectTransform>();
-                numTra[index]      = scoreObjTra[index].GetChild(0).GetComponent<RectTransform>();
-                numIma[index]      = new Image[maxDigit];
-                numIma[index][0]   = numTra[index].GetChild(0).GetComponent<Image>();
-                numIma[index][1]   = numTra[index].GetChild(1).GetComponent<Image>();
+                //ターゲットオブジェクト
+                targetObjTra[index]    = targetObj[index].GetComponent<RectTransform>();
+                targetNumTra[index]    = targetObjTra[index].GetChild(1).GetComponent<RectTransform>();
+                targetNumIma[index]    = new Image[maxDigit];
+                targetNumIma[index][0] = targetNumTra[index].GetChild(0).GetComponent<Image>();
+                targetNumIma[index][1] = targetNumTra[index].GetChild(1).GetComponent<Image>();
+
+                //スコアオブジェクト
+                scoreObjTra[index]    = scoreObj[index].GetComponent<RectTransform>();
+                scoreNumTra[index]    = scoreObjTra[index].GetChild(0).GetComponent<RectTransform>();
+                scoreNumIma[index]    = new Image[maxDigit];
+                scoreNumIma[index][0] = scoreNumTra[index].GetChild(0).GetComponent<Image>();
+                scoreNumIma[index][1] = scoreNumTra[index].GetChild(1).GetComponent<Image>();
             }
             harvestNum = new int[vegTypeNum];
+
+            //ステージ設定
+            tartgetVeg_         = tartgetVeg;            //目標野菜
+            targetVegetableNum_ = targetVegetableNum;    //目標野菜の個数
+            targetNum_          = targetNum;             //種類ごとの目標個数
+            stageNum_           = stageNum;              //ステージ番号
+            targetIndex = new int[targetVegetableNum_];  //目標野菜のインデクス番号
+            clearJudge  = new bool[targetVegetableNum_]; //クリア判定
+            int ten = 10;
+            for (int index = 0; index < targetVegetableNum_; index++)
+            {
+                int tensPlace = (int)Mathf.Floor(targetNum_[index] / ten); //10の位
+                int onesPlace = targetNum_[index] % ten;                   //1の位
+                targetIndex[index] = (int)tartgetVeg_[index];
+                targetNumIma[targetIndex[index]][0].sprite = targetNumSpr[onesPlace];
+                targetNumIma[targetIndex[index]][1].sprite = (tensPlace == 0) ? targetNumSpr[ten] : targetNumSpr[tensPlace];
+                clearJudge[index] = false;
+            }
         }
 
         //野菜収穫
@@ -62,8 +109,19 @@ namespace ShootMode
                 int onesPlace = harvestNum[vegIndex] % ten;                   //1の位
 
                 //数字更新
-                numIma[vegIndex][0].sprite = numberSpr[onesPlace];
-                numIma[vegIndex][1].sprite = (tensPlace == 0) ? numberSpr[ten] : numberSpr[tensPlace];
+                scoreNumIma[vegIndex][0].sprite = scoreNumSpr[onesPlace];
+                scoreNumIma[vegIndex][1].sprite = (tensPlace == 0) ? scoreNumSpr[ten] : scoreNumSpr[tensPlace];
+
+                //目標野菜収穫完了判断
+                int targetIndexIndex = Array.IndexOf(targetIndex, vegIndex);
+                if (targetIndexIndex >= 0 && targetNum_[targetIndexIndex] <= harvestNum[vegIndex])
+                {
+                    clearJudge[targetIndexIndex] = true;
+
+                    //クリア判定
+                    if (Array.IndexOf(clearJudge, false) < 0)
+                        Debug.Log("クリアですぞよ");
+                }
             }
         }
     }
--- Assets/Script/ShootMode/ShootModeManager.cs
+++ Assets/Script/ShootMode/ShootModeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using static ShootMode.ShootModeDefine;
 
 namespace ShootMode
@@ -25,9 +26,32 @@ namespace ShootMode
         [SerializeField]
         GameObject resultScreenPre;
 
+        public static VegetableType[] tartgetVeg;   //目標野菜
+        public static int targetVegetableNum;       //目標野菜の個数
+        public static int[] targetNum;              //種類ごとの目標個数
+        public static float blickGenerateTime;      //生成時間
+        public static int stageNum;                 //ステージ番号
+        public static bool tutorial;        　      //チュートリアル？
+        string[] vegName;                           //野菜の名前enum
+
         void Awake()
         {
             FlagReset();
+
+            //enum取得
+            var vegetableType = Enum.GetValues(typeof(VegetableType));
+            vegName = new string[vegetableType.Length];
+            foreach (VegetableType vegeValue in vegetableType)
+            { vegName[(int)vegeValue] = Enum.GetName(typeof(VegetableType), vegeValue); }
+
+            //ステージ設定(仮)
+            tartgetVeg = new VegetableType[] { VegetableType.Broccoli };
+            targetVegetableNum = 1;
+            targetNum = new int[] { 1 };
+            blickGenerateTime = 15.0f;
+            stageNum = 0;
+            tutorial = false;
+            BLOCK_GENERATE_TIME = blickGenerateTime;
         }
 
         public IEnumerator GameOver()
